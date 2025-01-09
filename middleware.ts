e82bf8d4a7ff674e6fe1c630c01f2a1a2196a0ea
Asim Rauf <asim.rauf@bitsol.tech>
2025-01-09 14:37:47 +0500

    feat: Remove Home page and update middleware for route protection and redirection

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)']);
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/forum(.*)',
+  '/',
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
     // Add custom logic to run before redirecting
-
     return NextResponse.redirect(new URL('/signin', req.url));
+  }
+
+  if (userId && req.nextUrl.pathname === '/') {
+    // Redirect logged-in users from home page to dashboard
+    return NextResponse.redirect(new URL('/dashboard', req.url));
   }
 });
 
